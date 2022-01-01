4ae310293e4f8a399f8536e6b347fcf2dbc1fc80
spell-landon <spell.landon@gmail.com>
2022-01-01 17:23:36 -0600

    General cleanup

--- src/App.js
+++ src/App.js
@@ -13,30 +13,29 @@ function App() {
   const [searchString, setSearchString] = useState('');
   const [lastSearch, setLastSearch] = useState('');
   const [show, setShow] = useState(false);
-    // useEffect(() => {
-    //   fetch(
-    //     `http://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=${searchOptions.key}`
-    //   )
-    //     .then((res) => res.json())
-    //     .then((res) => {
-    //       setData(res);
-    //       console.log(res);
-    //       setLastSearch(searchString);
-    //       setSearchString('');
-    //     })
-    //     .catch((err) => {
-    //       console.error('Something went wrong...', err);
-    //     })
-    //     .finally(() => {
-    //       return;
-    //     });
-    // }, []);
+  // useEffect(() => {
+  //   fetch(
+  //     `http://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=${searchOptions.key}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setData(res);
+  //       console.log(res);
+  //       setLastSearch(searchString);
+  //       setSearchString('');
+  //     })
+  //     .catch((err) => {
+  //       console.error('Something went wrong...', err);
+  //     })
+  //     .finally(() => {
+  //       return;
+  //     });
+  // }, []);
 
   const searchOptions = {
     key: process.env.REACT_APP_WEATHER_KEY,
     api: `http://api.openweathermap.org/data/2.5/weather?q=${searchString}&units=imperial&appid=`,
   };
-
 
   function getWeather() {
     const url = `${searchOptions.api}${searchOptions.key}`;
@@ -63,8 +62,6 @@ function App() {
     getWeather(searchString);
     setShow(true);
   }
-
-
 
   return (
     <div className='App'>
